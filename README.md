65ac8a142be6f4ca7b4ec4717932b4d6094739f8
Wyatt Herkamp <wherkamp@gmail.com>
2024-08-17 16:03:29 -0400

    Project Rename

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Nitro Json Schema Forms
+# Nitro Json Schema Forms [![forthebadge](https://forthebadge.com/images/badges/made-with-typescript.svg)](https://forthebadge.com) [![npm version](https://badge.fury.io/js/nitro-jsf.svg)](https://badge.fury.io/js/nitro-jsf)
+
 
 A little library for parsing Json Schema and making a format that can be used to create JSON Forms
 
 ## Experimental
 
-This library is in early staged of development. 
+This library is in early staged of development.
--- test/BadgeTests.spec.ts
+++ test/BadgeTests.spec.ts
@@ -2,18 +2,18 @@ import * as fs from 'fs'
 import { parseJsonSchema } from '../src/lib.js'
 
 test('Parse Badge.json', () => {
-  let schema = fs.readFileSync('test/schemas/Badge.json', 'utf8')
-  let parsed = parseJsonSchema(schema)
+  const schema = fs.readFileSync('test/schemas/Badge.json', 'utf8')
+  const parsed = parseJsonSchema(schema)
   console.log(JSON.stringify(parsed, null, 2))
 })
 test('Evaluate Badge.json with empty object', () => {
-  let schema = fs.readFileSync('test/schemas/Badge.json', 'utf8')
-  let parsed = parseJsonSchema(schema)
-  let properties = parsed.getProperties({})
+  const schema = fs.readFileSync('test/schemas/Badge.json', 'utf8')
+  const parsed = parseJsonSchema(schema)
+  const properties = parsed.getProperties({})
   for (const property of properties) {
     console.log(property.debug())
   }
-  let validationErrors = parsed.validate({})
+  const validationErrors = parsed.validate({})
   for (const error of validationErrors) {
     console.log(error)
   }
